49b9d6b5777ba73b3bf63eb2811b64eb16e9199c
dianqr <diansayyidah358@gamil.com>
2025-05-29 19:08:39 +0700

    cari movie berhasil

--- js/script.js
+++ js/script.js
@@ -9,7 +9,23 @@ $('#search-button').on('click', function () {
             's': $('#search-input').val()
         },
         success: function() {
-            if (XResult.Response == "True") {
+            if(result.Response == "True") {
+                let movies = result.Search;
+
+                $.each(movies, function(i,data){
+                    $('#Movie-list').append(`<div class="card mb-3" style="width: 20rem;">
+                    <img src=`+ data.Poster +` class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">`+ data.Title +`</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">`+ data.Year + `</h6>
+                      <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal"data-id="` + data.imdbID +`">See Detail</a>
+                      
+                    </div>
+                  </div>`);
+
+               });
+                $('#search-input').val('');
+            
             } else {
                 $('movie-list').html(`
                     <div class="col">
